915a92e1aa06f4b9b9cb7f68e5da6ed4e796f36a
Outfrost <kotlet.bahn@gmail.com>
2016-11-01 21:36:15 +0100

    added javadoc comments, swapped com.sun.javaws.exceptions.InvalidArgumentException for java.lang.IllegalArgumentException, added negative unit price check upon creation of item

--- MiniWebStore/src/FixedPointFormat.java
+++ MiniWebStore/src/FixedPointFormat.java
@@ -5,8 +5,19 @@
  *  Date: 24 Oct 2016
  */
 
+/**
+ * Provides a static shortcut method for printing integers formatted with 2 decimal places.
+ *
+ * @author  Iwo Bujkiewicz
+ * @version 20161101
+ */
 public class FixedPointFormat {
 	
+	/**
+	 * Returns a string containing the provided number formatted to display 2 decimal places.
+	 * @param number    Number to be formatted
+	 * @return          Resulting text representation of the number
+	 */
 	public static String decimal2Places(long number) {
 		return number / 100L + "." + ((number % 100L < 10) ? "0" + number % 100L : number % 100L);
 	}
